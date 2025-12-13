40a6a390b3e0422fc72ca4f6abfe3bfd8837d45a
wangyf <824537320@qq.com>
2025-12-13 18:02:52 +0800

    feat: add load test config

--- src/config.rs
+++ src/config.rs
@@ -26,9 +26,19 @@ pub struct Server {
 }
 
 impl AppConfig {
+    #[cfg(not(feature = "test-util"))]
     pub fn try_load() -> Result<AppConfig, config::ConfigError> {
+        Self::try_load0("config/app.yaml")
+    }
+
+    #[cfg(feature = "test-util")]
+    pub fn try_load() -> Result<AppConfig, config::ConfigError> {
+        Self::try_load0("config/app-test.yaml")
+    }
+
+    fn try_load0(file: &str) -> Result<AppConfig, config::ConfigError> {
         let config = Config::builder()
-            .add_source(config::File::with_name("config/app.yaml"))
+            .add_source(config::File::with_name(file))
             .build()?
             .try_deserialize::<AppConfigInner>()?;
         Ok(AppConfig {
@@ -42,5 +52,28 @@ impl Deref for AppConfig {
 
     fn deref(&self) -> &Self::Target {
         &self.inner
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[cfg(feature = "test-util")]
+    #[test]
+    fn test_try_load_with_test_util_feature() {
+        // 测试当启用test-util特性时，是否能正确加载test配置文件
+        let config = AppConfig::try_load().unwrap();
+        // 验证配置中的端口是否与app-test.yaml中的配置一致
+        assert_eq!(config.server.port, 8081);
+    }
+
+    #[cfg(not(feature = "test-util"))]
+    #[test]
+    fn test_try_load_without_test_util_feature() {
+        // 测试当不启用test-util特性时，是否能正确加载默认配置文件
+        let config = AppConfig::try_load().unwrap();
+        // 验证配置中的端口是否与app.yaml中的配置一致
+        assert_eq!(config.server.port, 8080);
     }
 }
